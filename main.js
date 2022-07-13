53b3a56da0a41dc319e290782f713db7603149b2
Leandro Sousa <leandrosousa.work@gmail.com>
2022-07-13 16:06:49 -0300

    Repositório do Alura Midi

---
+++ main.js
@@ -0,0 +1,28 @@
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function(evento) {
+        if (evento.code === "Enter" || evento.code === "Space") {
+
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+}
