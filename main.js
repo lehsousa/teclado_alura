3a311a2a4564622ea56d834ebfb89b4c8171375a
Leandro Sousa <leandrosousa.work@gmail.com>
2022-07-14 19:25:43 -0300

    alterando o main.js

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio)
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play()
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
+const listaDeTeclas = document.querySelectorAll('.tecla')
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}` //template string
 
     tecla.onclick = function() {
-        tocaSom(idAudio);
-    };
+        tocaSom(idAudio)
+    }
 
     tecla.onkeydown = function(evento) {
-        if (evento.code === "Enter" || evento.code === "Space") {
-
-            tecla.classList.add('ativa');
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa')
         }
     }
 
     tecla.onkeyup = function() {
-        tecla.classList.remove('ativa');
+        tecla.classList.remove('ativa')
     }
 }
